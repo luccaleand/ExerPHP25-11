1c950bc1cbd5980c68cc70215a91b749b65e64d3
lucca leandro <114000619+luccaleand@users.noreply.github.com>
2022-11-25 11:33:53 -0300

    Update 4.php

--- 4.php
+++ 4.php
@@ -1,7 +1,8 @@
 ------nome e sua idade------
+<?php
 $E = readline("QUAL SEU NOME??");
 $F = readline("QUAL SUA IDADE??");
 if($F>=18){echo $E," é maior de 18 e tem ", $F, " Anos";}
     
     else{echo $E," não é maior de 18 e tem ", $F, " Anos";}
-
+?>
