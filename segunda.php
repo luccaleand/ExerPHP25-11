3096899c3947189ebdbae92b834d146d6b4f5f72
lucca leandro <114000619+luccaleand@users.noreply.github.com>
2022-11-25 11:32:03 -0300

    Update segunda.php

--- segunda.php
+++ segunda.php
@@ -1,7 +1,9 @@
 -----Faça um programa PHP que receba os valores A e B, imprima-os em ordem crescente em relação aos seus valores.------
+<?php
 $a = readline("coloque o primeiro valor ");
 $b = readline("coloque o segundo valor ");
   if ($a < $b)
     {echo $a, ", " ,$b;} 
   else 
     {echo $b, ", " ,$a;}
+?>
